f9f3fb9d46a74aee70167ed16ac6c2ba9079cbdd
rajatenzyme <rajatmittal2012@gmail.com>
2020-07-19 11:09:04 +0530

    DESCR

--- 10 Days of code/Ganesha's pattern.cpp
+++ 10 Days of code/Ganesha's pattern.cpp
@@ -67,3 +67,42 @@ int main() {
 	cout<<"*"<<endl;
 	return 0;
 }
+
+/*
+Problem link : - https://hack.codingblocks.com/app/practice/6/1055/problem
+
+
+Take as input N, an odd number (>=5) . Print the following pattern as given below for N = 7.
+
+*  ****
+*  *
+*  *
+*******
+   *  *
+   *  *
+****  *
+
+Input Format
+
+Enter value of N ( >=5 )
+Constraints
+
+5 <= N <= 99
+Output Format
+
+Print the required pattern.
+Sample Input
+
+7
+
+Sample Output
+
+*  ****
+*  *
+*  *
+*******
+   *  *
+   *  *
+****  *
+
+*/
