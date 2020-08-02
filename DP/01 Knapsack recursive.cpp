541f1b4dc631837af285bfae163b17831a04aefe
rajatenzyme <rajatmittal2012@gmail.com>
2020-08-02 17:57:35 +0530

    update

--- DP/01 Knapsack recursive.cpp
+++ DP/01 Knapsack recursive.cpp
@@ -1,24 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 //Naive Recursive approach of 0/1 knapsack
-int knapsack ( int wt[], int val[], int w, int n)
+int knapsack (int wt[], int val[], int w, int n)
 {
   //Base Condition
   if(n==0 || w==0)
-  {
     return 0;
-  }
   
   //checking from last element 
   if(wt[n-1]<=w)
-  {
     return max(val[n-1] + knapsack(wt, val, w-wt[n-1], n-1), knapsack(wt, val, w, n-1));
-  }
 
-  else if(wt[n-1]>w)
-  {
+  else
     return knapsack(wt, val, w, n-1);
-  }
+  
 }
 
 
